a438d6a7586541bdb9ebcd8eca0a9dfa32046a2a
yuhyunjaes <jjnhfjkifdg@gmail.com>
2025-11-15 23:58:36 +0900

    Calenote-Ui-Update

--- resources/js/Pages/Calenote/Notepad.jsx
+++ resources/js/Pages/Calenote/Notepad.jsx
@@ -3,17 +3,13 @@ import { useEffect, useState} from "react";
 import Loading from "@/Components/Elements/Loading.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import NotepadTitleSection from "@/Components/Calenote/Notepad/NotepadTitleSection.jsx";
-import NotepadFilterSection from "@/Components/Calenote/Notepad/NotepadFilterSection.jsx";
 export default function Notepad({ auth }) {
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     return (
         <>
             <Head title="Notepad"/>
             <div className="container mx-auto min-h-full px-5 py-16 overflow-y-auto space-y-5">
-                <NotepadTitleSection />
-                <NotepadFilterSection setLoading={setLoading} />
             </div>
             <Loading Toggle={loading}/>
         </>
